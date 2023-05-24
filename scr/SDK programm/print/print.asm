c8712f57af2da52c53dcb9431313fe2330ba2790
pulsar2105 <54115653+pulsar2105@users.noreply.github.com>
2023-05-24 18:31:23 +0200

    print is working

--- scr/SDK programm/print/print.asm
+++ scr/SDK programm/print/print.asm
@@ -7,7 +7,8 @@ extern WriteConsolew ; for console
 extern ExitProcess   ; for exit
 
 section .data
-    message dq 8, 'bonjour', 10, 0 ; we must add a single character at the end of the string
+    message dq 5, 'hello', 0 ; we must add a single character at the end of the string ¯\_(ツ)_/¯
+    last_chr dq 1, 10, 0
 
 section .bss
     written resq 1 ; display
@@ -18,45 +19,48 @@ section .text
     main:
     mov rbp, rsp; for correct debugging
 
-        mov rcx, -11    ; normal settings
-        call GetStdHandle
-
-        mov rcx, rax            ; handle
-        mov rdx, message + 8    ; message, +8 to skip the length field
-        mov r8, [message]              ; message length (and the final character)
-        inc r8
-        mov r9, written         ; written characters
-        push 0                  ; lpReserved
-        call WriteConsoleA
-
-        ; stack cleaning
-        pop rcx
+        ; we print message print(data, last_chr)
+        push message 
+        push last_chr  
+        call print
         
-        ; we print message                
-        push message
-        call print        
+        push message 
+        push last_chr  
+        call print
 
         xor ecx, ecx
         call ExitProcess
 
 
-    ; rsp+8 = data to display
-    print:
-        mov rcx, -11    ; normal settings
+    ; rsp+16 = data to display
+    ; rsp+8 = last character to display
+    print:       
+        ; data
+        mov rcx, -11            ; normal settings
         call GetStdHandle
 
         mov rcx, rax            ; handle
-        mov rdx, [rsp+8]    ; message, +8 to skip the length field
+        mov rdx, [rsp+16]       ; message, +8 to skip the length field
         add rdx, 8
-        mov r8, [rsp+8] ; message length (and the final character)
+        mov r8, [rsp+16]        ; message length (and the final character)
         mov r8, [r8]
-        inc r8
+        inc r8                  ; 
         mov r9, written         ; written characters
         push 0                  ; lpReserved
         call WriteConsoleA
-
         ; stack cleaning
         pop rcx
+        
+        ; if data is NULL character, 0
+        cmp qword [rsp+8], 0
+        je print_end
+        
+        ; last character
+        push qword [rsp+8]
+        push 0
+        call print
 
-        ret 8
+        ret 16
 
+    print_end:
+        ret 16
