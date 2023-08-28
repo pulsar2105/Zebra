cfbea1ffff41943569bf3e8886ccdf513c14aaaa
pulsar2105 <54115653+pulsar2105@users.noreply.github.com>
2023-08-28 19:33:48 +0200

    access prototype + recode
    abc[0] => work
    [1,2,3][0] => don't work

--- scr/compiler/parser.py
+++ scr/compiler/parser.py
@@ -39,9 +39,9 @@ def determine_lower_para_influences(tokens):
             influences.append(parentheses_influence)
 
     # if the level of influence of the brackets is greater than 0
-    # we ask to remove the extra brackets again
+    # ask to remove the extra brackets again
     if min(influences) > 0:
-        # we don't remove brackets if there is comma in there
+        # don't remove brackets if there is comma in there
         if min(influences) == 1:
             for i in range(len(tokens)):
                 if tokens[i] == "," and influences[i] == min(influences):
@@ -56,7 +56,7 @@ def determine_lower_para_influences(tokens):
 def search_min_priority(tokens, operators, operators_priority):
     tokens, min_para_influence, influences = determine_lower_para_influences(tokens)
 
-    # we check if there are any operators left
+    # check if there are any operators left
     good = False
     for token in tokens:
         if token in operators:
@@ -72,7 +72,7 @@ def search_min_priority(tokens, operators, operators_priority):
                     if tokens[len(tokens) - 1 - i] == operator and influences[len(tokens) - 1 - i] == min_para_influence:
                         return operator, len(tokens) - 1 - i
 
-# we search if tokens list are a big function, ex: ["sin", "(", "10", ")"]
+# search if tokens list are a big function, ex: ["sin", "(", "10", ")"]
 def if_one_function(tokens):
     # first check
     if type(tokens) == list and len(tokens) > 1:
@@ -87,16 +87,76 @@ def if_one_function(tokens):
                         influ -= 1
 
                     if influ == 0 and i == len(tokens) - 1:
-                        return True
-                    elif influ == 0 and i != len(tokens) - 1:
-                        return False
-            else:
-                return False
-        else:
-            return False
-    else:
-        return False
+                        # get arguments function
+                        arguments = []
+                        i = 2 # 2 because we skip the fonction name and the first "("
+                        check_point = 2
+                        while tokens[i] != ")":
+                            if tokens[i] == ",":
+                                # if there isn't an argument we replace it with "None"
+                                if tokens[check_point:i] != []:
+                                    arguments.append(tokens[check_point:i])
+                                else:
+                                    arguments.append("None")
 
+                                check_point = i+1
+                            i += 1
+
+                        if tokens[check_point:i] != []:
+                            arguments.append(tokens[check_point:i])
+
+                        return True, arguments
+
+                    elif influ == 0 and i != len(tokens) - 1:
+                        return False, None
+            else:
+                return False, None
+        else:
+            return False, None
+    else:
+        return False, None
+
+# search if tokens list are a access to a list or dictionnary, ex: ["my_list", "[", "10", "]"]
+def if_access(tokens):
+    # first check
+    if type(tokens) == list and len(tokens) > 1:
+        if type(tokens[0]) == str:
+            if re.match(r"\w+", tokens[0]) and tokens[1] == "[" and not tokens[0] in operators:
+                # if the last parenthesis is to the function
+                influ = 0
+                for i in range(1, len(tokens)):
+                    if tokens[i] == "[":
+                        influ += 1
+                    if tokens[i] == "]":
+                        influ -= 1
+
+                    if influ == 0 and i == len(tokens) - 1:
+
+                        # get arguments
+                        arguments = []
+                        i = 2 # 2 because we skip the fonction name and the first "("
+                        check_point = 2
+                        while tokens[i] != "]":
+                            if tokens[i] == ":":
+                                arguments.append(tokens[check_point:i])
+                                check_point = i+1
+                            i += 1
+
+                        if tokens[check_point:i] != []:
+                            arguments.append(tokens[check_point:i])
+
+                        return True, arguments
+
+                    elif influ == 0 and i != len(tokens) - 1:
+                        return False, None
+            else:
+                return False, None
+        else:
+            return False, None
+    else:
+        return False, None
+
+# search if tokens list are a big function, ex: ["sin", "(", "10", ")"]
 def is_list_dict(tokens, opening_character, closing_character):
     # control check (to avoid error)
     if type(tokens) == list and len(tokens) > 1:
@@ -106,18 +166,18 @@ def is_list_dict(tokens, opening_character, closing_character):
             if tokens[1] == closing_character:
                 return True, []
             else:
-                # we get the list/dict/tuple elements
+                # get the list/dict/tuple elements
                 arguments = []
                 influ = 1
                 check_point = 1
                 i = 1
                 while influ != 0 and i != len(tokens):
-                    # we add an argument (little or big) to the list
+                    # add an argument (little or big) to the list
                     if tokens[i] in [",", "=>", closing_character] and influ == 1:
                         arguments.append(tokens[check_point:i])
                         check_point = i+1
 
-                    # we stop at the end off arguemnts ex: (10,) for avoid null arguement
+                    # stop at the end off arguemnts ex: (10,) for avoid null arguement
                     if tokens[i] == "," and tokens[i+1] == closing_character:
                         break
 
@@ -134,6 +194,7 @@ def is_list_dict(tokens, opening_character, closing_character):
     else:
         return False, None
 
+# search if tokens list are a tuple, ex: ["(", "10", ",", "25", ")"]
 def is_tuple(tokens):
     # control check (to avoid error)
     if type(tokens) == list and len(tokens) > 1:
@@ -154,18 +215,24 @@ def is_tuple(tokens):
             if tokens == ["(", ",", ")"]:
                 return True, []
             else:
-                # we get the tuple elements
+                # get the tuple elements
                 arguments = []
                 influ = 1
                 check_point = 1
                 i = 1
                 while influ != 0 and i != len(tokens):
-                    # we add an argument (little or big) to the list
+                    # add an argument (little or big) to the list
                     if tokens[i] in [",", ")"] and influ == 1:
-                        arguments.append(tokens[check_point:i])
+
+                        # if there isn't an argument we replace it with "None"
+                        if tokens[check_point:i] != []:
+                            arguments.append(tokens[check_point:i])
+                        else:
+                            arguments.append("None")
+
                         check_point = i+1
 
-                    # we stop at the end off arguemnts ex: (10,) for avoid null arguement
+                    # stop at the end off arguemnts ex: (10,) for avoid null arguement
                     if tokens[i] == "," and tokens[i+1] == ")":
                         break
 
@@ -186,27 +253,15 @@ def is_tuple(tokens):
 # Parser
 def line_parser(tokens):
 
-    # we detect floats number to avoid parsing "."
+    # detect floats number to avoid parsing "."
     if type(tokens) == str:
         return tokens
 
-    # we check for the presence of functions
-    if if_one_function(tokens):
-        # we get the function and its arguments
+    # check for the presence of functions
+    if if_one_function(tokens)[0]:
+        # get the function and its arguments
         action = tokens[0]
-
-        # get arguments
-        arguments = []
-        i = 2 # 2 because we skip the fonction name and the first "("
-        check_point = 2
-        while tokens[i] != ")":
-            if tokens[i] == ",":
-                arguments.append(tokens[check_point:i])
-                check_point = i+1
-            i += 1
-
-        if tokens[check_point:i] != []:
-            arguments.append(tokens[check_point:i])
+        arguments = if_one_function(tokens)[1]
 
         # arguments are parsed
         for i in range(len(arguments)):
@@ -215,10 +270,10 @@ def line_parser(tokens):
         # "*" is for unpack all elements
         return [action, *arguments]
 
-    # we check for the presence of tuple
+    # check for the presence of tuple
     elif is_tuple(tokens)[0]:
         action = "tuple"
-        true_false, arguments = is_tuple(tokens)
+        arguments = is_tuple(tokens)[1]
 
         # arguments are parsed
         for i in range(len(arguments)):
@@ -227,10 +282,10 @@ def line_parser(tokens):
         # "*" is for unpack all elements
         return [action, *arguments]
 
-    # we check for the presence of list
+    # check for the presence of list
     elif is_list_dict(tokens, "[", "]")[0]:
         action = "list"
-        true_false, arguments = is_list_dict(tokens, "[", "]")
+        arguments = is_list_dict(tokens, "[", "]")[1]
 
         # arguments are parsed
         for i in range(len(arguments)):
@@ -239,10 +294,10 @@ def line_parser(tokens):
         # "*" is for unpack all elements
         return [action, *arguments]
 
-    # we check for the presence of dict
+    # check for the presence of dict
     elif is_list_dict(tokens, "{", "}")[0]:
         action = "dict"
-        true_false, arguments = is_list_dict(tokens, "{", "}")
+        arguments = is_list_dict(tokens, "{", "}")[1]
 
         # arguments are parsed
         for i in range(len(arguments)):
@@ -251,8 +306,21 @@ def line_parser(tokens):
         # "*" is for unpack all elements
         return [action, *arguments]
 
+    # check for the presence of access
+    elif if_access(tokens)[0]:
+        action = "access"
+
+        arguments = if_access(tokens)[1]
+
+        # arguments are parsed
+        for i in range(len(arguments)):
+            arguments[i] = line_parser(arguments[i])
+
+        # "*" is for unpack all elements
+        return [action, tokens[0], *arguments]
+
     # operators part
-    # we find the operator who is made in LAST
+    # find the operator who is made in LAST
     operator, pos = search_min_priority(tokens, operators, operators_priority)
 
     tokens, min_para_influence, influences = determine_lower_para_influences(tokens)
@@ -280,14 +348,14 @@ def line_parser(tokens):
 
 data = "b = -(10 - value * (--3*32) - sin(5*10-19, 2) + (-a) + fact(10) * cos(10 + 1) * 'abcde' - 10 - sun.mass)"
 
-# we check if there are errors natively in the line
+# check if there are errors natively in the line
 err.string_error(data)
-# we do the syntactic analysis
+# do the syntactic analysis
 tokens = lex.line_lexer(data)
 print(tokens)
 # the syntax is checked
 err.errors(tokens)
-# we create the instruction tree
+# create the instruction tree
 tree = line_parser(tokens)
 print(tree)
 
